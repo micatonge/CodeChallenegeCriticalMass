af1896204e1a823f6c2dc40c858001b6a7e75525
Mica Tonge <micatonge@10-16-146-3.dynapool.wireless.nyu.edu>
2019-05-08 20:15:54 -0400

    need to fix shark button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+
 
 const song = `Baby Shark doo doo doo doo doo doo
 Baby Shark doo doo doo doo doo doo
@@ -21,4 +21,21 @@ Grandma Shark doo doo doo doo doo doo
 Grandma Shark doo doo doo doo doo doo
 Grandma Shark!`;
 
-
+//splits the song into an array of each song line
+var songLines = song.split("\n");
+//initializing string to hold the updates lyric lines
+var songLyricString = "";
+//initializing index variable
+var i = 0;
+//displays each updated lyric line list
+function displayLyricLine(){
+  //adds current lyric line to string
+  songLyricString += "<li>" + songLines[i] +"</li>";
+  //updates the documentElement that displayes the current lyric list
+  document.getElementById("lyrics-list").innerHTML = songLyricString;
+  //for loop that runs through each line in the array
+  if (++i < songLines.length) {
+     //delays 1.5sec and recalls the function to display the current list
+      setTimeout(displayLyricLine, 1500); //ms
+  }
+}
